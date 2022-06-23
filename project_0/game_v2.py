73ebde81ece2e63ae4e31d6a70c9551fbe21bf5f
FLeonProg <f.leon.prog@gmail.com>
2022-06-23 14:08:36 +0300

    PEP 8

--- project_0/game_v2.py
+++ project_0/game_v2.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math 
+
+
 def random_predict(number:int=1) -> int:
     """Рандомно угадываем число
 
@@ -10,25 +12,26 @@ def random_predict(number:int=1) -> int:
         int: Число попыток
     """
 
-    count = 0
-    x_1 = 1
-    x_2 = 100
-    x = math.floor((x_1 + x_2)/2) # предполагаемое число
+    count = 0 # счётчик
+    min_num = 1 # минимально возможное число
+    max_num = 100 # максимально возможное число
+    num = math.floor((min_num+max_num) / 2) # предполагаемое число
 
     while True:
         count += 1
-        if (x == number):
+        if (num == number):
             break  # выход из цикла, если угадали
-        if (x > number):
-            x_2 = math.ceil(((x_1 + x_2)/2))
-            x = math.floor((x_1 + x_2)/2)
+        if (num > number):
+            max_num = math.ceil(((min_num + max_num) / 2))
+            num = math.floor((min_num + max_num) / 2)
         else:
-            x_1 = math.floor((x_1 + x_2)/2) 
-            x = math.ceil((x_1 + x_2)/2)
+            min_num = math.floor((min_num + max_num) / 2) 
+            num = math.ceil((min_num + max_num) / 2)
         if (count > 100):
             break
 
     return(count)
+
 
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем из 1000 подходов угадывает наш алгоритм
@@ -49,7 +52,7 @@ def score_game(random_predict) -> int:
 
     score = int(np.mean(count_ls)) # находим среднее количество попыток
 
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
+    print(f'Алгоритм угадывает число в среднем за: {score} попыток')
     return(score)
 
 # RUN
